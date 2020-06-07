3dd9fea15d40a54e81c415dd411a4ada6c145890
Santosh Mainali <cyx977@outlook.com>
2020-06-07 19:59:15 +0545

    color for keys problem

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -34,38 +36,75 @@ class TransactionList extends StatelessWidget {
           })
         : ListView.builder(
             itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: EdgeInsets.all(7),
-                      child: FittedBox(
-                        child: Text(
-                          "\$${transactions[index].amount}",
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    "${transactions[index].title}",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    "${DateFormat.yMMMd().format(transactions[index].date)}",
-                  ),
-                  trailing: IconButton(
-                    onPressed: () {
-                      removeTransaction(id: transactions[index].id);
-                    },
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                  ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: transactions[index],
+                  removeTransaction: removeTransaction);
             },
             itemCount: transactions.length,
           );
+  }
+}
+
+class TransactionItem extends StatefulWidget {
+  const TransactionItem({
+    Key key,
+    @required this.transaction,
+    @required this.removeTransaction,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final Function removeTransaction;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgcolor;
+  @override
+  void initState() {
+    super.initState();
+    const colors = [
+      Colors.green,
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+    ];
+    _bgcolor = colors[Random().nextInt(4)];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: _bgcolor,
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(7),
+            child: FittedBox(
+              child: Text(
+                "\$${widget.transaction.amount}",
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          "${widget.transaction.title}",
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          "${DateFormat.yMMMd().format(widget.transaction.date)}",
+        ),
+        trailing: IconButton(
+          onPressed: () {
+            widget.removeTransaction(id: widget.transaction.id);
+          },
+          icon: Icon(Icons.delete),
+          color: Theme.of(context).errorColor,
+        ),
+      ),
+    );
   }
 }
